503811e8f759d87a2470a905b12e40a4c2cbf627
JBloodBeardN <jtn.neese@gmail.com>
2019-05-03 13:10:11 -0500

    initial commit from project 2 copy/paste

---
+++ models/cats.js
@@ -0,0 +1,75 @@
+//pasted from project 2, needs updating for project 3
+//does not need petType
+//need primary key for this table
+//association to bridge table
+
+module.exports = function (sequelize, DataTypes) {
+  
+    var pets = sequelize.define("pets", {
+      petId:
+      {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      petName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      imageURL: {
+        type: DataTypes.STRING
+      },
+      birthMonth: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      birthYear:
+      {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      petType:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      petSubtype:
+      {
+        type: DataTypes.STRING,
+      },
+      gender:
+      {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      neutered:
+      {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      medicalHistory:
+      {
+        type: DataTypes.TEXT,
+      }
+    });
+    pets.associate = function (models) {
+      pets.belongsTo(models.owners, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      pets.hasMany(models.petNotes, {
+        onDelete: "cascade"
+      });
+      pets.hasOne(models.dogImmunizations, {
+        onDelete: "cascade"
+      });
+      pets.hasOne(models.catImmunizations, {
+        onDelete: "cascade"
+      });
+    };
+    return pets;
+  
+  };
---
+++ models/petsBridge.js
@@ -0,0 +1,5 @@
+//this needs petID as primary key
+//foreignKey of Dog table
+//foreignKey of Cat table
+//petType
+//needs ownerId foreign key
