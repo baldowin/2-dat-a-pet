30628025cc0d8a3f843595b0a524a6f26c22e151
Nancy <nancy.dragon.zapata@gmail.com>
2019-05-07 14:02:29 -0500

    dded forms folder

---
+++ src/pages/components/forms/sign_up/sign_up.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      toolbarEnabled: true
+    });
+  });
+
+  // Or with jQuery
+
+  $('.fixed-action-btn').floatingActionButton({
+    toolbarEnabled: true
+  });
