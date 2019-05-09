9c5fb243d176c18f277002983c53b66a284df8de
JBloodBeardN <jtn.neese@gmail.com>
2019-05-09 13:04:41 -0500

    after fixing dashboard and admin sequelize calls

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var passport = require("./config/passport.js");
+var passport = require("./config/passport");
 var db = require("./models");
 var express = require("express");
 var session = require("express-session");
-var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3001;
+require("dotenv").config();
 //tion stuff need to be here
 
 // var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -17,30 +17,20 @@ app.use(session({ secret: "jamesCat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 var syncOptions = { force: false };
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+}else{
+app.use(express.static("public"));
 }
 
-app.use(express.static("public"));
+require("./routes/apiRoutes")(app);
 
-// Define API routes here
-//Project 2- does this route need to be a server route for authentication
-
-app.get("/login", function (req, res) {
-  if (req.user) {
-    res.redirect("/dashboard");
-  } else {
-    // res.render("login");
-    res.render("/signup");
-  }
-});
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+
 
 // app.listen(PORT, () => {
 //   console.log(`🌎 ==> API server now on port ${PORT}!`);
